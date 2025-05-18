3445ace2ce52cc42144eab567f8dc9267d446c0a
raffaela09 <rafaelafranco096@gmail.com>
2025-05-17 20:13:05 -0400

    deliveryMan e ManagerBusiness quase completos

--- DeliveryMan.py
+++ DeliveryMan.py
@@ -1,57 +1,39 @@
-from Service import Service
+from speedBox_izab3lla import User
 from Exceptions import ProductNotFoundError
-class DeliveryMan:
-    def __init__(self):
-        pass
-    def make_delivery():
-        ...
-    def collect_delivery():
-        ...
-        
-
-class DeliveryManManager(Service):
-    def __init__(self, folder, file_json):
-        super().__init__(folder, file_json)
-                     
-    def pick_up_order(self, code):
-       self.change_status(
-           code = code,
-           status = 'Pronto para entrega',
-           status_alterado = 'Em rota para entrega',
-           mensagem = f'O pedido {code} foi aceito e está em rota para entrega.',
-           error = ProductNotFoundError,
-           msg_error = 'Nenhum produto com esse código encontrado.'
-           
-       )
-        
-    def deliver_order(self, code):
-        self.change_status(
-            code = code,
-            status = 'Em rota para entrega',
-            status_alterado = 'Pedido entregue.',
-            mensagem = f'O pedido {code} foi entrego e pago com sucesso!',
-            error = ProductNotFoundError,
-            msg_error = 'Nenhum produto com esse código encontrado.'
-        )
+from save_json import load_orders, update_json
+class DeliveryMan(User):
+    def __init__(self, name, cpf, email, pwd, user_type):
+        super().__init__(name, cpf, email, pwd, user_type)
+    #-----------------------------------------------------------------------------------
+      
+    def make_delivery(self, num_order):
+        orders = load_orders()
+        for order in orders:
+            if num_order == order["num_order"] and order["status"] == "out for delivery":
+                update_json(
+                    update_orders={
+                        "num_order": num_order, 
+                        "status": "order delivered"
+                    },
+                    keys=["num_order"]
+                )
+            else:
+                pass
+    #-----------------------------------------------------------------------------------
     
-    def show_history_delivery(self):
-        orders = self.load_data()
-        orders_route = []
-        orders_finally = []
-        
-        if orders:
-            for order in orders:
-                if order['status'] == 'Pedido em rota para entrega':
-                    orders_route.append(order)
-                if order['status'] == 'Pedido entregue':
-                    orders_finally.append(order)
-        if orders_route:
-            print('Pedidos em rota pra entrega:\n')
-            for item in orders_route:
-                self.product_details(item)
-        if orders_finally:
-            print('Pedidos finalizados:\n')
-            for item in orders_finally:
-                self.product_details(item)            
-        if not orders_finally and not orders_route:
-            raise ProductNotFoundError('Nenhum pedido encontrado.')
+    def collect_delivery(self, num_order):
+        orders = load_orders()
+        for order in orders:
+            if num_order == order["num_order"] and order["status"] == "awaiting pickup":
+                update_json(
+                    update_orders={
+                        "num_order": num_order, 
+                        "status": "out for delivery"
+                    },
+                    keys=["num_order"]
+                )
+            else:
+                pass
+    #-----------------------------------------------------------------------------------
+
+  
--- ManagerBusiness.py
+++ ManagerBusiness.py
@@ -1,39 +1,50 @@
-from Service import Service
 from Exceptions import ProductNotFoundError
-
-class ManagerBusiness:
-    def __init__(self):
-        pass
-    def schedule_delivery():
-        ...
-    def refuse_delivery():
-        ...
+from speedBox_izab3lla import User
+from save_json import load_orders, update_json
+class ManagerBusiness(User):
+    def __init__(self, name, cpf, email, pwd, user_type):
+        super().__init__(name, cpf, email, pwd, user_type)
+    #----------------------------------------------------------------------------------- 
+    '''funcao responsável por aceitar um pedido, irá verificar de acordo com o código do pedido se está no json e com o status correto (nesse caso, status de em espera),
+    caso esteja, ele chama a funcao de atualizar os dados do json, que passa o que vai ser atualizado e por qual chave ele deve procurar dentro do json para atulizar, caso contrario, ele levanta uma excecao.
+    ''' 
+    def schedule_delivery(self, num_order):
+        orders = load_orders()
+        for order in orders:
+            if num_order == order["num_order"] and order["status"] == "on hold":
+                update_json(
+                    update_orders={
+                        "num_order": num_order, 
+                        "status": "awaiting pickup"
+                    },
+                    keys=["num_order"]
+                )
+            else:
+                pass
+    #-----------------------------------------------------------------------------------
+    
+    
+    
+    '''Funcao responsavel por cancelar um pedido, ela funciona da mesma forma que a anterior, verifica atravez do codigo do pedido se esta no json e com o status em espera, caso esteja, ele chama a funcao de atualizar os dados no json, que recebe os dados que vao ser
+    atualizados e por qual chave deve-se procurar dentro do json para que possa ser atualizado, caso contrario, ele levanta uma excecao
+    '''             
+    def refuse_delivery(self, num_order):
+        orders = load_orders()
+        for order in orders:
+            if num_order == order["num_order"] and order["status"] == "on hold":
+                update_json(
+                    update_orders={
+                        "num_order": num_order, 
+                        "status": "canceled"
+                    },
+                    keys=["num_order"]
+                )
+                break
+        else:
+            pass
+    #-----------------------------------------------------------------------------------
+            
     def evaluate_delivery():
         ...
         
-#aqui nesse caso herda do service, mas posso mudaar isso
-class BusinessManager(Service):
-    def __init__(self, folder, file_json):
-        super().__init__(folder, file_json) 
-        
-    def accept_order(self, code):
-         self.change_status(
-           code = code,
-           status = 'Em espera',
-           status_alterado = 'Pronto para entrega',
-           mensagem = f'O pedido {code} foi aceito e está aguardando coleta.',
-           error = ProductNotFoundError,
-           msg_error = 'Nenhum produto com esse código encontrado.'
-           
-       )
-         
-    def reject_order(self, code):
-        self.change_status(
-           code = code,
-           status = 'Em espera',
-           status_alterado = 'Cancelado',
-           mensagem = f'O pedido {code} foi cancelado pela loja.',
-           error = ProductNotFoundError,
-           msg_error = 'Nenhum produto com esse código encontrado.'
-           
-       )
+
